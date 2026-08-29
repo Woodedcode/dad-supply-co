7ee377e0a90963135ad98d8cbf8e06fa3b53a7db
Ryan Woods <Woodedcode@gmail.com>
2026-08-29 15:58:36 -0500

    working on product page layout

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,7 @@
 import DadSupplyTee from "../assets/dad-supply-tee.png";
 import DadSupplyHat from "../assets/dad-supply-hat.png";
-import ImpastaTee from "../assets/impasta-back.png";
+import ImpastaFront from "../assets/impasta-front.png";
+import ImpastaBack from "../assets/impasta-back.png";
 
 const products = [
     {
@@ -23,7 +24,8 @@ const products = [
         id: 3,
         name: "Im-pasta Tee",
         price: "$28.00",
-        image: ImpastaTee,
+        image:ImpastaFront,
+        backImage: ImpastaBack,
         type: "shirt",
         sizes: ["S", "M", "L", "XL", "2XL"],
 },
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,16 +1,52 @@
-return (
-  <main className="product-page">
-    <div className="product-page__images">
-      <img src={product.image} alt={product.name} />
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import products from "../data/products";
 
-      {product.backImage && (
-        <img src={product.backImage} alt={`${product.name} back`} />
-      )}
-    </div>
+function ProductPage() {
+  const { id } = useParams();
 
-    <div className="product-page__info">
-      <h1>{product.name}</h1>
-      <p>{product.price}</p>
-    </div>
-  </main>
-);
+  const product = products.find(
+    (product) => String(product.id) === id
+  );
+
+  const [selectedImage, setSelectedImage] = useState(product?.image);
+
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
+
+  return (
+    <main className="product-page">
+      <div className="product-page__thumbnails">
+  <button onClick={() => setSelectedImage(product.image)}>
+    <img
+      src={product.image}
+      alt={`${product.name} front`}
+    />
+  </button>
+
+  {product.backImage && (
+    <button onClick={() => setSelectedImage(product.backImage)}>
+      <img
+        src={product.backImage}
+        alt={`${product.name} back`}
+      />
+    </button>
+  )}
+</div>
+
+<img
+  className="product-page__main-image"
+  src={selectedImage}
+  alt={product.name}
+/>
+
+      <div className="product-page__info">
+        <h1>{product.name}</h1>
+        <p>{product.price}</p>
+      </div>
+    </main>
+  );
+}
+
+export default ProductPage;
